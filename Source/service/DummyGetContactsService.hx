e3793d19057caffc2089354e23e6b3e0cb1e69a9
Robin Burrer <info@burrer.de>
2012-11-11 16:01:39 +0100

    clean up

--- Source/service/DummyGetContactsService.hx
+++ Source/service/DummyGetContactsService.hx
@@ -1,3 +1,25 @@
+/*
+Copyright (c) Burrer UI design and development
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished to do 
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+*/
+
 package service;
 import vo.ContactVO;
 /**
@@ -16,9 +38,10 @@ class DummyGetContactsService implements IGetContactsService
 	public  function getContacts(r:IResponder):Void		
 	{
 		var contactsArray:Array<ContactVO> = new Array<ContactVO>();
+		contactsArray.push(new ContactVO("Bart", "Simpson", "01.png"));
 		contactsArray.push(new ContactVO("Homer", "Simpson", "03.png"));
 		contactsArray.push(new ContactVO("Maggie", "Simpson", "02.png"));
-		contactsArray.push(new ContactVO("Bart", "Simpson", "01.png"));
+		
 		
 	
 	
